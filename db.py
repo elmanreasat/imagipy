d4fa199f0a7e8b5e2bbe8868ad0b89562599964b
Elman Reasat <elmanreasat@hotmail.com>
2021-05-07 11:28:52 -0400

    fixed same image upload error

--- db.py
+++ db.py
@@ -5,4 +5,5 @@ db = SQLAlchemy()
 def db_init(app):
     db.init_app(app)
     with app.app_context():
+        #db.drop_all()
         db.create_all()
--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from db import db
 
 
 class Img(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    img = db.Column(db.Text, unique=True, nullable=False)
+    id = db.Column(db.Integer, unique=True, primary_key=True, )
+    img = db.Column(db.Text, nullable=False)
     name = db.Column(db.Text, nullable=False)
     mimetype = db.Column(db.Text, nullable=False)
