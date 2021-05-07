ad4dbeb299fdb4d9b74ae2281cd320092ba336a9
Elman Reasat <elmanreasat@hotmail.com>
2021-05-06 22:02:50 -0400

    init commit 2

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from werkzeug.utils import secure_filename
+from db import db_init, db
+from models import Img
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///image.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db_init(app)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/upload', methods=['POST'])
+def upload():
+    pic = request.files['pic']
+    if not pic:
+        return 'No pic uploaded!', 400
+
+    filename = secure_filename(pic.filename)
+    mimetype = pic.mimetype
+    if not filename or not mimetype:
+        return 'Bad upload!', 400
+
+    img = Img(img=pic.read(), name=filename, mimetype=mimetype)
+    db.session.add(img)
+    db.session.commit()
+
+    return 'Img Uploaded!', 200
