1752e7210f60fc19309ed6e52d9b59ec2e00665c
Elman Reasat <elmanreasat@hotmail.com>
2021-05-07 11:53:36 -0400

    multiple image uploads and check image type

--- app.py
+++ app.py
@@ -14,17 +14,18 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    pic = request.files['pic']
-    if not pic:
+    pic_list = request.files.getlist("pics")
+    #import pdb; pdb.set_trace()
+    if not pic_list:
         return 'No pic uploaded!', 400
+    for pic in pic_list:
+        filename = secure_filename(pic.filename)
+        mimetype = pic.mimetype
+        if not filename or not mimetype or "image" not in str(mimetype):
+            return 'Bad upload!', 400
 
-    filename = secure_filename(pic.filename)
-    mimetype = pic.mimetype
-    if not filename or not mimetype:
-        return 'Bad upload!', 400
-
-    img = Img(img=pic.read(), name=filename, mimetype=mimetype)
-    db.session.add(img)
-    db.session.commit()
+        img = Img(img=pic.read(), name=filename, mimetype=mimetype)
+        db.session.add(img)
+        db.session.commit()
 
     return 'Img Uploaded!', 200
